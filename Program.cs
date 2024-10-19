7e8f25fd98d9ececf021d88920fcaab4b3d791b3
DanilaT <danila.tyapkin@gmail.com>
2024-10-19 15:41:09 +0300

    Fexed

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Git
             Console.WriteLine("Newline");
             Console.WriteLine("Nice commit");
             Console.WriteLine("Hmmm its true?");
-            Console.WriteLine("Danger")
+            Console.WriteLine("Danger"); //fix
             Console.WriteLine("there is an error in the previous line");
             Console.WriteLine("Have you corrected the error?");
         }
